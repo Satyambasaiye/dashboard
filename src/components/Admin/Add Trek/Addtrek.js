41780c7cb665837ad0618080d689a18200c21bac
Satyam Basaiye <satyambasaiye123@gmail.com>
2023-04-14 11:07:38 +0530

    react-quill issue resolved and added new input row with + and - buttons

--- src/components/Admin/Add Trek/Addtrek.js
+++ src/components/Admin/Add Trek/Addtrek.js
@@ -1,298 +1,294 @@
-import React, { useState } from "react";
-import axios from "axios";
-import ReactQuill from "react-quill";
-// import './index.css'
+import React, { useState } from 'react';
+import axios from 'axios';
+import ReactQuill from 'react-quill';
 
-// import 'react-quill/dist/quill.snow.css';
+import 'react-quill/dist/quill.snow.css';
 
-// const modules = {
-//   toolbar: [
-//     [{ header: "1" }, { header: "2" }, { font: [] }],
-//     [{ size: [] }],
-//     ["bold", "italic", "underline", "strike", "blockquote"],
-//     [
-//       { list: "ordered" },
-//       { list: "bullet" },
-//       { indent: "-1" },
-//       { indent: "+1" },
-//     ],
-//     ["link", "image", "video"],
-//     ["clean"],
-//   ],
-//   clipboard: {
-//     matchVisual: false,
-//   },
-// };
+const modules = {
+  toolbar: [
+    [{ 'header': '1'}, {'header': '2'}, {'font': []}],
+    [{size: []}],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+    [{'list': 'ordered'}, {'list': 'bullet'},     {'indent': '-1'}, {'indent': '+1'}],
+    ['link', 'image', 'video'],
+    ['clean']
+  ],
+  clipboard: {
+    matchVisual: false,
+  },
+}
 
-// const formats = [
-//   "header",
-//   "font",
-//   "size",
-//   "bold",
-//   "italic",
-//   "underline",
-//   "strike",
-//   "blockquote",
-//   "list",
-//   "bullet",
-//   "indent",
-//   "link",
-//   "image",
-//   "video",
-// ];
+const formats = [  'header', 'font', 'size',  'bold', 'italic', 'underline', 'strike', 'blockquote',  'list', 'bullet', 'indent',  'link', 'image', 'video']
 
-const Form = () => {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [isEdit, setIsEdit] = useState(false);
+const Addtrek = () => {
+  const [eventName, seteventName] = useState('');
+  const [displayName, setdisplayName] = useState('');
+  const [description, setDescription] = useState('');
+  const [visibility, setVisibility] = useState('public');
 
-  const [locations, setLocations] = useState("");
-  const [fromDate, setFromDate] = useState("");
-  const [toDate, setToDate] = useState("");
-  const [actualPrice, setActualPrice] = useState("");
-  const [discountedPrice, setDiscountedPrice] = useState("");
+
+  const [locations, setLocations] = useState('');
+  const [fromDate, setFromDate] = useState('');
+  const [toDate, setToDate] = useState('');
+  const [actualPrice, setActualPrice] = useState('');
+  const [discountedPrice, setDiscountedPrice] = useState('');
   const [image, setImage] = useState(null);
-  const [itinerary, setItinerary] = useState("");
-  const [thingsToCarry, setThingsToCarry] = useState("");
-  const [pickUpPoint, setPickUpPoint] = useState("");
-  const [includes, setIncludes] = useState("");
-  const [contactDetails, setContactDetails] = useState("");
-  const [cancellationPolicy, setCancellationPolicy] = useState("");
+  const [pickUpPoint, setPickUpPoint] = useState('');
+  const [rows, setRows] = useState([{ name: '', count: '', discount: '',ticket:'', date: '' }]);
 
+  const addRow = () => {
+    setRows([...rows, { name: '', count: '', discount: '',ticket:'', date: '' }]);
+  };
+
+  const deleteRow = (index) => {
+   if (rows.length > 1){ const newRows = [...rows];
+    newRows.splice(index, 1);
+    setRows(newRows);}
+  };
+  const handleInputChange = (event, rowIndex, fieldName) => {
+    const newRows = [...rows];
+    newRows[rowIndex][fieldName] = event.target.value;
+    setRows(newRows);
+  };
   const handleDescriptionChange = (value) => {
     setDescription(value);
-  };
+  }
 
-  const handleItineraryChange = (value) => {
-    setItinerary(value);
-  };
+  const handlePickUpPointChange = (event) => {
+    setPickUpPoint(event.target.value);
+  }
 
-  const handleThingsToCarryChange = (value) => {
-    setThingsToCarry(value);
-  };
+ 
+  function handleVisibilityChange(event) {
+    setVisibility(event.target.value);
+  }
 
-  const handlePickUpPointChange = (value) => {
-    setPickUpPoint(value);
-  };
-
-  const handleIncludesChange = (value) => {
-    setIncludes(value);
-  };
-
-  const handleContactDetailsChange = (value) => {
-    setContactDetails(value);
-  };
-
-  const handleCancellationPolicyChange = (value) => {
-    setCancellationPolicy(value);
-  };
 
   const handleChange = (value) => {
     setDescription(value);
-  };
+  }
+  
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     const data = {
-      title,
+      eventName,
+      displayName,
       description,
       locations,
       fromDate,
       toDate,
       actualPrice,
       discountedPrice,
-      itinerary,
-      thingsToCarry,
       pickUpPoint,
-      includes,
-      contactDetails,
-      cancellationPolicy,
-    };
-
+      rows
+    }
+    
     const config = {
       headers: {
-        "Content-Type": "application/json",
-      },
-    };
+        'Content-Type': 'application/json'
+      }
+    }
 
     try {
-      const response = await axios.post("/api/products", data, config);
-    } catch (error) {}
+      const response = await axios.post('/api/products', data, config);
+      console.log(response.data);
+
+    } catch (error) {
+    }
   };
-  return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        background: "",
-      }}
-    >
-      <form
-        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-        onSubmit={handleSubmit}
-      >
-        <h1
-          className="text-4xl text-center mb-8 font-bold text-gray-800"
-          style={{ width: "100%" }}
-        >
-          Trek Form
-        </h1>
-        <div className="mb-4">
-          <label
-            className="block font-bold text-gray-700"
-            htmlFor="title"
-            style={{ display: "block" }}
-          >
-            Title
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline "
-            id="title"
-            type="text"
-            placeholder="Title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
+return(
+  <div className="cbg-gradient-to-br from-blue-500 to-purple-500 rounded-lg p-6">
+  
+    <form className="bg-gray-100 p-6 rounded-lg" onSubmit={handleSubmit} >
+    <div className="text-center mb-8">
+  <h1 className="text-4xl font-bold text-gray-800">Trek Form</h1>
+  
+</div>       
+ <div className="mb-4">
+            <label className="block font-bold text-gray-700 mb-2" htmlFor="eventName">Event Name:</label>
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-2" id="eventName" type="text" placeholder="Enter Event Name" value={eventName} onChange={(e) => seteventName(e.target.value)}   />
+            
         </div>
 
-        <div className="mb-6">
-          <label className="block font-bold text-gray-700">Description:</label>
-          {/* <ReactQuill
-            className="w-full border-gray-300 rounded-md shadow-sm px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-            value={description}
-            onChange={handleChange}
-            modules={modules}
-            formats={formats}
-            theme="snow"
-            placeholder="Enter description here"
-            required
-          /> */}
-        </div>
-        <div className="my-4 flex flex-col">
-          <label className="block font-bold text-gray-700 mb-2">
-            Locations:
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            rows="4"
-            value={locations}
-            onChange={(e) => setLocations(e.target.value)}
-            required
-          />
-        </div>
-        <div className="grid grid-cols-2 gap-4 mb-6">
-          <div>
-            <label className="block font-bold text-gray-700">From Date:</label>
-            <input
-              className="block w-full border-gray-300 rounded-md shadow-sm px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-              type="date"
-              value={fromDate}
-              onChange={(e) => setFromDate(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label className="block font-bold text-gray-700">To Date:</label>
-            <input
-              className="block w-full border-gray-300 rounded-md shadow-sm px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-              type="date"
-              value={toDate}
-              onChange={(e) => setToDate(e.target.value)}
-              required
-            />
-          </div>
-        </div>
-        <div className="grid grid-cols-2 gap-4 mb-6">
-          <div>
-            <label className="block font-bold text-gray-700">
-              Actual Price:
-            </label>
-            <input
-              className="block w-full border-gray-300 rounded-md shadow-sm px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-              type="number"
-              value={actualPrice}
-              onChange={(e) => setActualPrice(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label className="block font-bold text-gray-700">
-              Discount Price:
-            </label>
-            <input
-              className="block w-full border-gray-300 rounded-md shadow-sm px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-              type="number"
-              value={discountedPrice}
-              onChange={(e) => setDiscountedPrice(e.target.value)}
-              required
-            />
-          </div>
-        </div>
         <div className="mb-4">
-          <label className="block">Upload Image:</label>
-          <input
-            className="block w-full border-gray-300 rounded-md shadow-sm"
-            type="file"
-            onChange={(e) => setImage(e.target.files[0])}
-          />
+            <label className="block font-bold text-gray-700 mb-2" htmlFor="displayName">Event Display Name:</label>
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-2" id="displayName" type="text" placeholder="Enter Event Name" value={displayName} onChange={(e) => setdisplayName(e.target.value)}   />
         </div>
-        <div className="mb-4">
-          <label className="block">Itinerary:</label>
+        <div className="custom-margin">
+            <label className="block font-bold text-gray-700 mb-2">Event Location:</label>
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-2" type="text" rows="4" placeholder="Enter Event Location" value={locations} onChange={(e) => setLocations(e.target.value)} required  />
         </div>
-        <div className="mb-4">
-          <label className="block">Things to carry:</label>
-        </div>
-        <div className="mb-4">
-          <label className="block">Pick up points:</label>
-        </div>
+  <div className="my-4">
+          <label className='block font-bold text-gray-700 mb-2'>Event Visibilit:</label>
+          <div>
+          <input type="radio" id="public" name="visibility" value="public" checked={visibility === 'public'} onChange={handleVisibilityChange} />
 
-        <div className="my-4 flex flex-col">
-          <label className="block font-bold text-gray-700 mb-2">
-            Cost Includes:
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            rows="4"
-            value={includes}
-            onChange={(e) => setIncludes(e.target.value)}
-            required
-          />
+            <label htmlFor="public" className="form-label">Public</label>
+          </div>
+          <div>
+          <input type="radio" id="link" name="visibility" value="link" checked={visibility === 'link'} onChange={handleVisibilityChange} />
+
+            <label htmlFor="link" className="form-label">Link</label>
+          </div>
+          <div>
+          <input type="radio" id="private" name="visibility" value="private" checked={visibility === 'private'} onChange={handleVisibilityChange} />
+            <label htmlFor="private" className="form-label">Private</label>
+          </div>
         </div>
-        <div className="my-4 flex flex-col">
-          <label className="block font-bold text-gray-700 mb-2">
-            Contact Details:
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            rows="4"
-            value={contactDetails}
-            onChange={(e) => setContactDetails(e.target.value)}
-            required
-          />
-        </div>
-        <div className="my-4 flex flex-col">
-          <label className="block font-bold text-gray-700 mb-2">
-            Cancellation Policy:
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            rows="4"
-            value={cancellationPolicy}
-            onChange={(e) => setCancellationPolicy(e.target.value)}
-            required
-          />
-        </div>
-        <button className="bg-blue-500 text-white py-2 px-4 rounded-md shadow-sm hover:bg-blue-600 transition-colors duration-300">
-          Submit
-        </button>
-      </form>
+  
+
+  <div className="grid grid-cols-2 gap-4 mb-6">
+    <div>
+      <label className="block font-bold text-gray-700">From Date:</label>
+      <input
+        className="block w-full border-gray-300 rounded-md shadow-sm px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+        type="date"
+        value={fromDate}
+        style={{maxWidth: '300px'}}
+        onChange={(e) => setFromDate(e.target.value)}
+        required
+      />
     </div>
-  );
-};
-export default Form;
+    <div>
+      <label className="block font-bold text-gray-700">To Date:</label>
+      <input
+        className="block w-full border-gray-300 rounded-md shadow-sm px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+        type="date"
+        value={toDate}
+        style={{maxWidth: '300px'}}
+        onChange={(e) => setToDate(e.target.value)}
+        required
+      />
+    </div>
+  </div>
+  <div className="grid grid-cols-2 gap-4 mb-6">
+    <div>
+      <label className="block font-bold text-gray-700">Actual Price:</label>
+      <input
+        className="block w-full border-gray-300 rounded-md shadow-sm px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+        type="number"
+        value={actualPrice}
+        placeholder="0"
+        style={{maxWidth: '300px'}}
+
+        onChange={(e) => setActualPrice(e.target.value)}
+        required
+      />
+    </div>
+    <div>
+      <label className="block font-bold text-gray-700">Discount Price:</label>
+      <input
+        className="block w-full border-gray-300 rounded-md shadow-sm px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+        type="number"
+        value={discountedPrice}
+        placeholder="0"
+        style={{maxWidth: '300px'}}
+
+        onChange={(e) => setDiscountedPrice(e.target.value)}
+        required
+      />
+    </div>
+      </div>
+      <div className="mb-4">
+        <label className="block">Event Image:</label>
+        <input
+          className="block w-full border-gray-300 rounded-md shadow-sm"
+          type="file"
+          id="image"
+          onChange={(e) => setImage(e.target.files[0])}
+        />
+      </div>
+      <div className="mb-6">
+    <label className="block font-bold text-gray-700">Description:</label>
+    <ReactQuill 
+      className="w-full border-gray-300 rounded-md shadow-sm px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+      value={description} 
+      onChange={handleDescriptionChange}
+      modules={modules}
+      formats={formats}
+      theme="snow"
+      rows={5}
+      placeholder="Enter description here"
+      required
+    />
+  </div>
+  <div className="mb-6">
+  <label className="block font-bold text-gray-700">Pick up points:</label>
+  <textarea
+    className="w-full border-gray-300 rounded-md shadow-sm px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+    value={pickUpPoint}
+    onChange={handlePickUpPointChange}
+    placeholder="Enter pick up points here"
+  />
+</div>
+
+        {rows.map((row, rowIndex) => (
+        <div key={rowIndex} style={{ marginBottom: '20px' }}>
+          <input
+          
+            type="text"
+            class=" mr-2"
+            placeholder="Ticket name"
+            value={row.name}
+            onChange={(event) => handleInputChange(event, rowIndex, 'name')}
+          />
+          <input
+            type="number"
+            className=" mr-2"
+            placeholder="Total ticket count"
+            value={row.count}
+            onChange={(event) => handleInputChange(event, rowIndex, 'count')}
+          />
+          <input
+            type="number"
+            className=" mr-2"
+            placeholder="Discount price"
+            value={row.discount}
+            onChange={(event) => handleInputChange(event, rowIndex, 'discount')}
+          />
+          <input
+            type="number"
+            className=" mr-2"
+            placeholder="Ticket price"
+            value={row.ticket}
+            onChange={(event) => handleInputChange(event, rowIndex, 'ticket')}
+          />
+          <input
+            type="date"
+            className=" mr-2"
+            placeholder="Date"
+            value={row.date}
+            onChange={(event) => handleInputChange(event, rowIndex, 'date')}
+          />
+           <button type="button" onClick={addRow}
+  className="bg-blue-500 hover:bg-blue-700 text-white py-0.3 px-1.5 rounded-full mr-2"
+  >
+        +
+      </button>
+      <button type="button" onClick={() => deleteRow(rowIndex)} 
+        className="bg-blue-500 hover:bg-blue-700 text-white py-0.2 px-1.5 rounded-full mr-2"
+>
+            -
+          </button>
+        </div>
+      ))}
+     
+
+     <div style={{ display: 'flex', justifyContent: 'center' }}>
+      
+
+        <button className="bg-green-500 text-white py-2 px-4 rounded-md shadow-sm hover:bg-green-600 transition-colors duration-300 " style={{ marginTop: '20px' }}>
+        Add Trek
+        </button>
+        </div>
+          </form>
+          </div>
+          );
+          
+          
+          
+  
+  }  
+  export default Addtrek;
